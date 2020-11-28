98d83524449d251ff2c3b172cc6ee5600f6eaa11
Lofia <72675909+Lofia@users.noreply.github.com>
2020-11-27 19:14:03 -0600

    Adding regression code

--- code/Graph_drawing.R
+++ code/Graph_drawing.R
@@ -52,3 +52,26 @@ M=M/as.integer(unname(table(raw$stars)))
 
 par(mfrow=c(2,4))
 for(i in 1:length(u)) barplot(M[,i],main=names(u)[i],xlab='Stars')
+################################################# regress
+
+city=stream_in('business_city_Chinese.json')
+stars=city$stars
+mo=city$hours.Monday
+tu=city$hours.Tuesday
+we=city$hours.Wednesday
+th=city$hours.Thursday
+fr=city$hours.Friday
+sa=city$hours.Saturday
+su=city$hours.Sunday
+week=cbind(mo,tu,we,th,fr,sa,su)
+com=matrix(nrow=751,ncol=7)
+for(i in 1:7) com[,i]=as.integer(complete.cases(week[,i]))
+opendays=rowSums(com)
+always=which(opendays==7)
+useless=which(opendays==0)
+stars=stars[-useless]
+opendays=opendays[-useless]
+# opendays=ifelse(opendays==7,yes=1,no=0)
+
+l=lm(stars~opendays)
+summary(l)
